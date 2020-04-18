a19ea310fbed0c45325f50b263ab7e6e7ccfad4d
Sven Huff <info@sven-huff.de>
2020-04-18 14:29:17 +0200

    seeting_up_functions.php

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php 
+function myBlog_files() {
+  wp_enqueue_script('main-blog-js', get_theme_file_uri('/js/index.js'), NULL, '1.0', true);
+  wp_enqueue_style('custom-google-fonts', '//https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700');
+   wp_enqueue_style('style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'myBlog_files');
+
+function myBlog_features() {
+  register_nav_menu('headerMenuLocation', 'Header Menu Location');
+}
+
+add_action('after_setup_theme', myBlog_features);
+
+
+add_filter( 'excerpt_length', function($length) {
+    return 15;
+} );
+
+?>
