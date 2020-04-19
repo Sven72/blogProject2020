a87551a11a12959db5034e3cfff4007038f26655
Sven Huff <info@sven-huff.de>
2020-04-19 18:44:20 +0200

    post thumbnails

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ function myBlog_features() {
   register_nav_menu('headerMenuLocation', 'Header Menu Location');
 add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+
 }
 
 add_action('after_setup_theme', myBlog_features);
--- ideen.md
+++ ideen.md
@@ -2,3 +2,4 @@
 #2. GitHub Account verknüpfen.
 #3. Date | Author -> METAs integrieren
 #4. Beitragsbilder anpassen mit unterschiedlichen Größen
+https://themecoder.de/2016/11/23/wie-du-beitragsbilder-im-wordpress-theme-implementieren-kannst/
