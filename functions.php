bee096cbf15a06bc73d10f6e84b67310afcaa70a
Sven Huff <info@sven-huff.de>
2020-05-25 19:14:28 +0200

    comment on function

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 function myBlog_files() {
+  // true platziert das Script im Footer
   wp_enqueue_script('main-blog-js', get_theme_file_uri('/js/index.js'), NULL, '1.0', true);
    wp_enqueue_style( 'font-awesome', '//use.fontawesome.com/releases/v5.3.1/css/all.css', array(), '5.3.1' );
   wp_enqueue_style('custom-google-fonts', '//https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700');
