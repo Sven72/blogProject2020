84f1ad771442047997e908fe42666db56d304392
Sven Huff <info@sven-huff.de>
2020-06-05 15:10:34 +0200

    Fixed issue with featured image in single

--- single.php
+++ single.php
@@ -4,18 +4,17 @@
     ?>
      
     <section class="main-wrapper">
+
 <?php 
       while(have_posts()) {
-    the_post(); ?>
+    the_post(); 
     
+ 
+    ?>
+ 
+ 
 <article class="article main-article_single">
-  
-    <?php
-if ( has_post_thumbnail() ) { 
-the_post_thumbnail( 'full' );
-}
-?>
-
+<?php the_post_thumbnail(array(700, 700) ); ?>
   <div class="metabox">
       <p class="article-cat"><?php echo get_the_category_list(', '); ?></p>
         <p>Post von <?php the_author_posts_link(); ?> am <?php the_time('j.n.y'); ?></p>
@@ -36,31 +35,7 @@ the_post_thumbnail( 'full' );
         </ul>   
      </div>
 
-<?php 
 
- $comments = get_comments();
-
-if ( have_comments() ) : ?>
-    <h3 id="comments-title"><?php
-        printf(
-            _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'twentyten' ),
-            number_format_i18n( get_comments_number() ),
-            '<em>' . get_the_title() . '</em>'
-        );
-    ?></h3>
-// [and more, of course...]
-<?php else : // or, if we don't have comments:
-    if ( ! comments_open() ) : ?>
-        <p class="nocomments"><?php _e( 'Comments are closed.', 'twentyten' ); ?></p>
-    <?php endif; // end ! comments_open() ?>
-<?php endif; // end have_comments() ?>
-
-      <div class="comments" style="margin-top: 100px">
-         <?php
-    if(comments_open() ):
-        comments_template();
-    endif;
-    ?>
     </div>
 
 
